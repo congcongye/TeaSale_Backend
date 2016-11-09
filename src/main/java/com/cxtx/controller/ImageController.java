8790fe7edeb398ab62114da1ba4f69807ef90f4c
ase16_yecc <ase16_yecc@qq.com>
2016-11-09 23:43:45 +0800

    商品图片的增删改查

--- src/main/java/com/cxtx/controller/ImageController.java
+++ src/main/java/com/cxtx/controller/ImageController.java
@@ -1,15 +1,28 @@
 package com.cxtx.controller;
 
+import com.cxtx.dao.ProductDao;
+import com.cxtx.entity.Image;
+import com.cxtx.entity.Product;
+import com.cxtx.model.CreateImageModel;
 import com.cxtx.model.CreateManagerModel;
+import com.cxtx.model.DeleteImageModel;
 import com.cxtx.model.ServiceResult;
 import com.cxtx.service.ImageService;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by ycc on 16/11/6.
@@ -19,41 +32,92 @@ public class ImageController extends  ApiController{
 
     @Autowired
     private ImageService imageService;
+    @Autowired
+    private ProductDao productDao;
 
     /**
-     * 上传logo文件,上传成功后，可以拿到上传的文件名
+     * 上传logo文件,上传成功后，可以拿到上传的文件名(image新增和修改)
      * @param pictures
-     * @param product_id
-     * @param
+     * @param model
      * @return
      * @throws IOException
      */
     @RequestMapping(value = "/image/upload", method = RequestMethod.POST)//,produces = "text/plain;charset=UTF-8"
     @ResponseBody
-    public ServiceResult uploadLogo(@RequestParam("picture") MultipartFile [] pictures,
-                             @RequestParam("product_id") Long product_id) throws IOException {//, HttpServletRequest request
-          int  succCount= imageService.uploadImages(pictures,product_id);
+    public ServiceResult uploadLogo(@RequestParam("pictures") MultipartFile [] pictures,
+                                   @RequestParam(value = "product_id",defaultValue = "-1")Long product_id,
+                                    @RequestParam(value = "image_id",defaultValue = "-1")Long image_id,@RequestParam(value = "type",defaultValue = "0")int type) throws IOException {//, HttpServletRequest request
+        checkParameter(pictures!=null,"pictures are empty");
+        checkParameter(pictures.length!=0,"pictures are empty");
+        Product product=productDao.findByIdAndAlive(product_id,1);
+        checkParameter(product!=null,"product is empty");
+        int succCount= imageService.uploadImages(pictures,product,image_id,type);
         if(succCount!=pictures.length){
             return ServiceResult.fail(500, "the num of succeed is "+succCount+" ; the fail number is "+(pictures.length-succCount));
+        }
+        return ServiceResult.success("all succeed ");
+    }
+
+    /**
+     * 通过url获取图片
+     * @param response
+     * @param url
+     * @throws IOException
+     */
+    @RequestMapping(value = "/image/getByUrl", method = RequestMethod.GET)
+    @ResponseBody
+    public void downloadLogo(HttpServletResponse response,
+                             @RequestParam(value="url") String url) throws IOException {// @PathVariable(value="url") String url
+        OutputStream os = response.getOutputStream();
+        try {
+            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("cxtx.properties");
+            Properties p = new Properties();
+            p.load(inputStream);
+            String folderPath = p.getProperty("logoPath");
+            response.reset();
+            File file = new File(folderPath+File.separator+url);
+            response.setContentType("application/octet-stream; charset=UTF-8");
+            os.write(FileUtils.readFileToByteArray(file));
+            os.flush();
+        } finally {
+            if(null != os) {
+                os.close();
+            }
+        }
+    }
+
+    /**
+     * 图片的批量删除
+     * @param list
+     * @return
+     */
+    @RequestMapping(value = "/image/delete", method = RequestMethod.PUT)
+    @ResponseBody
+    public ServiceResult startSell(@RequestBody List<DeleteImageModel> list){
+        checkParameter(list!=null&&!list.isEmpty(),"images are empty");
+        int succCount = imageService.delete(list);
+        if(succCount!=list.size()){
+            return ServiceResult.fail(500, "the num of succeed is "+succCount+" ; the fail number is "+(list.size()-succCount));
         }
         return ServiceResult.success("all succeed");
     }
 
     /**
-     *
-     * @param headPic
+     * 通过产品获得所有的图片
+     * @param product_id
      * @return
-     * @throws Exception
      */
-    @RequestMapping(value = "/image/headpic/register", method = RequestMethod.POST)
+    @RequestMapping(value = "/image/getImageByProduct", method = RequestMethod.GET)
     @ResponseBody
-    public ServiceResult register(@RequestParam("headPic") MultipartFile headPic) throws Exception{
-
-        String headUrl = imageService.uploadImage(headPic);
-        if (headUrl == null){
-            //TODO default headURL
-        }
-
-        return ServiceResult.success(headUrl);
+    public ServiceResult getAllByProduct(@RequestParam (value="product_id",defaultValue = "-1")Long product_id,
+                                         @RequestParam(value = "type",defaultValue = "-1")int type){
+        Product product =productDao.findByIdAndAlive(product_id,1);
+        checkParameter(product!=null,"product is empty");
+        List<Image> list =imageService.getAllByProductAndTypeAndAlive(product,type,1);
+        return ServiceResult.success(list);
     }
+
+
+
+
 }
--- src/main/java/com/cxtx/service/ImageService.java
+++ src/main/java/com/cxtx/service/ImageService.java
@@ -1,16 +1,20 @@
 package com.cxtx.service;
 
+import com.cxtx.entity.Image;
+import com.cxtx.entity.Product;
+import com.cxtx.model.DeleteImageModel;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by ycc on 16/10/30.
  */
 public interface ImageService {
 
-    int uploadImages(MultipartFile pictures[],Long product_Id) throws IOException;
-
-    String uploadImage(MultipartFile multipartFile) throws IOException;
+     int uploadImages(MultipartFile pictures[], Product product, Long image_id,int type) throws IOException;
+     int delete (List<DeleteImageModel> list);
+     List<Image> getAllByProductAndTypeAndAlive(Product product,int type,int alive);
 }
