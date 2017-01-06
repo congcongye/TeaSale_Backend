501caf8ddfe63e0cf78c002196b2081840a23614
金初阳 <2468231855@qq.com>
2017-01-06 14:55:19 +0800

    解决冲突

--- src/main/java/com/cxtx/hello/ScheduledTasks.java
+++ src/main/java/com/cxtx/hello/ScheduledTasks.java
@@ -1,13 +1,19 @@
 package com.cxtx.hello;
 
+import com.cxtx.predictor.Predictor;
 import com.cxtx.service.CrowdFundingService;
 import com.cxtx.service.CrowdSourcingService;
+import jxl.read.biff.BiffException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by jinchuyang on 16/12/6.
@@ -23,5 +29,25 @@ public class ScheduledTasks {
     public void reportCurrentTime() {
         crowdFundingService.checkNum();
         crowdSourcingService.checkNum();
+    }
+
+    /**
+     * 每天24点00分00秒时执行
+     * @throws IOException
+     * @throws BiffException
+     */
+    @Scheduled(cron = "50 27 23 * * ?")
+    public void timerCron() throws IOException, BiffException {
+        //System.out.println("current time : "+ sdf.format(new Date()));
+        DecimalFormat df   = new DecimalFormat("######0.00");
+        String[] types ={"West Lake Longjing","Tieguanyin","Biluochun"};
+        Map<String, Double> result = new HashMap<String, Double>();
+        Predictor predictor = new Predictor();
+        for (String type : types) {
+            double price = predictor.Predicte(type);
+            result.put(type, Double.parseDouble(df.format(price)));
+        }
+        String json = com.alibaba.fastjson.JSON.toJSONString(result,true);
+
     }
 }
